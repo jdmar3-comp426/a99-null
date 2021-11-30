5109ee657908bf83f8b21fff2e92362710b01d9b
Peter Wi <peter.h.wi96@gmail.com>
2021-11-30 19:36:31 +0900

    modified firebase-config address

--- frontend/frontend/src/components/data.js
+++ frontend/frontend/src/components/data.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react'
 import axios from 'axios'
 
-import {db, auth} from './firebase-config'
+import {db, auth} from '../firebase-config'
 import { getAuth } from "firebase/auth";
 import { doc, updateDoc, arrayUnion, arrayRemove, serverTimestamp } from "firebase/firestore";
 
